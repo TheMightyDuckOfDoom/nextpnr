e70c1da29b1005dc23cd17c80fd9d6370817475a
Tobias Senti <git@tsenti.li>
2025-08-28 23:59:28 +0200

    xcxk: rebase fixes

--- generic/viaduct/xcxk/test/viaduct_blinky.sh
+++ generic/viaduct/xcxk/test/viaduct_blinky.sh
@@ -3,4 +3,4 @@
 yosys -p "tcl ../projectXCxk/synth/scripts/synth.tcl ../../../examples/blinky.v top ./"
 
 # Run PnR
-~/nextpnr/nextpnr-generic --uarch xcxk --json top_synth.json -o device=3020 $1 --write pnr_blinky.json
+~/nextpnr/build/nextpnr-generic --uarch xcxk --json top_synth.json -o device=3020 $1 --write pnr_blinky.json
